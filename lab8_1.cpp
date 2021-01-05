cf5743ecd2f7c6738155e2da97cc019eb1d0ca51
limthelimn <nuttawan.lim@gmail.com>
2021-01-05 17:31:31 +0700

    lab8/1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,33 +1,45 @@
 #include<iostream>
-#include<iomanip> //For using setw(), setprecision(), ...
+#include<iomanip>
 using namespace std;
 
-int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+int main(){
+    double loan ,rate,amount;
+    cout << "Enter initial loan: ";
+    cin >> loan;
+    cout << "Enter interest rate per year (%): ";
+    cin >> rate;
+    cout << "Enter amount you can pay per year: ";
+    cin >> amount;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
-	cout << setw(13) << left << "Interest"; 
-	cout << setw(13) << left << "Total";
-	cout << setw(13) << left << "Payment";
-	cout << setw(13) << left << "NewBalance";
-	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
-	return 0;
+
+    cout << setw(13) << left << "EndOfYear#"; 
+    cout << setw(13) << left << "PrevBalance"; 
+    cout << setw(13) << left << "Interest"; 
+    cout << setw(13) << left << "Total";
+    cout << setw(13) << left << "Payment";
+    cout << setw(13) << left << "NewBalance";
+    cout << "\n";
+
+    int year = 1;
+    double inter,total,newbalance;
+    double balance = loan,payment = amount;
+
+    for(int i = 0;balance > 0;i++){
+        inter = (rate*balance)/100;
+        total = balance + inter;
+
+        if(total < payment){
+            payment = total;
+    	}
+        newbalance = total - payment;
+
+        cout << setw(13) << left << setprecision (2) << year++;
+        cout << setw(13) << left << setprecision (2) << fixed << balance;
+        cout << setw(13) << left << setprecision (2) << fixed << inter;
+        cout << setw(13) << left << setprecision (2) << fixed << total;
+        cout << setw(13) << left << setprecision (2) << fixed << payment;
+        cout << setw(13) << left << setprecision (2) << fixed << newbalance << endl;
+        balance = newbalance;
+    }
+    return 0;
 }
